f4f30984e9e2d6c54c153cd6aa57e8bf948844e8
giorgio poggini <giorgio.poggini@gmail.com>
2024-03-20 22:46:33 +0100

    Update

--- cmd/prometheus-mqtt-sd/util/config.go
+++ cmd/prometheus-mqtt-sd/util/config.go
@@ -1,28 +1,28 @@
 package util
 
 import (
-    "os"
-
-    "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
+	"os"
 )
 
 type SdConfig struct {
-    Address string `yaml:"address"`
-    Topic   string `yaml:"topic"`
+	Address  string `yaml:"address"`
+	Topic    string `yaml:"topic"`
+	ClientID string `yaml:"client_id"`
 }
 
 func LoadConfig(file string) (*SdConfig, error) {
-    var config SdConfig
+	var config SdConfig
 
-    configFile, err := os.ReadFile(file)
-    if err != nil {
-        return nil, err
-    }
+	configFile, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
 
-    err = yaml.Unmarshal(configFile, &config)
-    if err != nil {
-        return nil, err
-    }
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		return nil, err
+	}
 
-    return &config, nil
+	return &config, nil
 }
--- cmd/prometheus-mqtt-sd/util/discovery.go
+++ cmd/prometheus-mqtt-sd/util/discovery.go
@@ -1,31 +1,103 @@
 package util
 
 import (
-	"log"
+	"context"
+	"encoding/json"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/pkg/errors"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/discovery/targetgroup"
 )
 
 
 type discovery struct {
-	// ...
 	topic  string
 	client mqtt.Client
-	logger log.Logger
 }
 
 
-func (config *SdConfig)InitDiscovery() (*discovery, error) {
-	opts := mqtt.NewClientOptions().AddBroker(config.Address)
-	opts.SetClientID("prometheus-mqtt-sd")
-	// Create a new MQTT client
-	client := mqtt.NewClient(opts)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		return nil, token.Error()
+func (config *SdConfig) InitDiscovery() (*discovery, error) {
+	client, err := config.initClient()
+	if err != nil {
+		return nil, errors.WithStack(err)
 	}
 	cd := &discovery{
 		topic:  config.Topic,
 		client: client,
 	}
 	return cd, nil
+}
+
+func (d *discovery) Run(ctx context.Context, ch chan<- []*targetgroup.Group) {
+	// Create a channel to handle incoming MQTT messages
+	msgChan := make(chan *mqtt.Message)
+
+	// Set up the MQTT message handler
+	d.client.Subscribe(d.topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+		msgChan <- &msg
+	})
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case msg := <-msgChan:
+			// Process the MQTT message to create a targetgroup.Group
+			tg, err := d.processMessage(msg)
+			if err != nil {
+				continue
+			}
+
+			// Send the target group to the provided channel
+			ch <- tg
+		}
+	}
+}
+
+func (d *discovery) processMessage(msg *mqtt.Message) ([]*targetgroup.Group, error) {
+	// TODO: Implement this function to process the MQTT message and create a targetgroup.Group
+	// You will likely need to parse the message payload and possibly use additional metadata
+	// from the MQTT message.
+
+	g := []struct {
+		Targets []string          `json:"targets"`
+		Labels  map[string]string `json:"labels"`
+	}{}
+
+	// deserialize the message payload into the struct g
+	if err := json.Unmarshal((*msg).Payload(), &g); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	println("Processing message: ", g)
+	println("message lenght: ", len(g) )
+	tgs := make([]*targetgroup.Group, 0, len(g))
+
+	for _, group := range g {
+		targets := make([]model.LabelSet, 0, len(group.Targets))
+		for _, t := range group.Targets {
+			targets = append(targets, model.LabelSet{
+				model.AddressLabel: model.LabelValue(t),
+			})
+		}
+		labels := make(model.LabelSet, len(group.Labels))
+		for k, v := range group.Labels {
+			labels[model.LabelName(k)] = model.LabelValue(v)
+		}
+		if err := labels.Validate(); err != nil {
+			return nil, errors.WithStack(err)
+		}
+
+		tgs = append(tgs, &targetgroup.Group{
+			Targets: targets,
+			Labels:  labels,
+			Source: group.Labels["job"],
+		})
+		println("tgs: ", tgs)
+		println("tgs lenght: ", len(tgs) )
+		println("targets: ", tgs[0].Targets)
+	}
+	return tgs, nil
+
 }
